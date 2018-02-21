bea975cf93fa4dabecb5c2e801c7ff37498415a4
Thomas James <tj@tjej.uk>
2018-02-21 22:26:48 +0000

    Cleanup

--- n1mmlistener/Program.cs
+++ n1mmlistener/Program.cs
@@ -2,14 +2,12 @@
 using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace n1mmlistener
