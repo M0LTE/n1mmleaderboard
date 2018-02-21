822c4397904648373454be3a5891f874e83396ef
Thomas James <tj@tjej.uk>
2018-02-21 22:56:07 +0000

    Publish for win10 platform, still not quite right but closer

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,3 @@
 @echo off
-dotnet publish n1mmlistener -r win-x64 -o ..\pub\win-x64
-dotnet publish n1mmlistener -r win-x86 -o ..\pub\win-x86
+dotnet publish n1mmlistener -r win10-x64 -o ..\pub\win10-x64
+dotnet publish n1mmlistener -r win10-x86 -o ..\pub\win10-x86
